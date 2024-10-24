0079059163d2694ace6679294376d863a9204573
Drew McIntosh <refreshertowel@gmail.com>
2024-10-25 00:31:00 +1000

    Add files via upload

---
+++ PillowShading.lua
@@ -0,0 +1,144 @@
+local sprite = app.site.sprite
+if sprite.selection.isEmpty then
+	return app.alert("Selection empty!")
+end
+
+local data
+local dlg = Dialog():number{ id="max_iterations", label="Max Iterations (0 to perform the maximum possible iterations):", text="0", decimals=0 }
+dlg:modify{ title = "Pillow Shader" }
+dlg:number{ id="iteration_size", label="Iteration size:", text="1", decimals=0 }
+dlg:newrow()
+dlg:radio { id="selection_circle", label = "Selection settings", text = "Circle", selected = true,
+	onclick = function()
+		dlg:modify {
+			id = "selection_rectangle",
+			selected = false
+		}
+	end
+} 
+dlg:newrow()
+dlg:radio { id="selection_rectangle", label = "", text = "Rectangle", selected = false,
+	onclick = function()
+		dlg:modify {
+			id = "selection_circle",
+			selected = false
+		}
+	end
+}
+dlg:newrow()
+dlg:newrow()
+dlg:radio { id="contract", label="", text="Shrink", selected = true,
+	onclick = function()
+		dlg:modify {
+			id = "expand",
+			selected = false
+		}
+	end
+}
+dlg:newrow()
+dlg:radio { id="expand", label="", text="Expand", selected = false,
+	onclick = function()
+		dlg:modify {
+			id = "contract",
+			selected = false
+		}
+	end
+}
+dlg:button{ id="confirm", text="Confirm" }
+dlg:button{ id="cancel", text="Cancel" }
+dlg:show()
+data = dlg.data
+if data.confirm then
+	local iterations = 0
+	local brush_type = "circle"
+	local size_change = dlg.data.iteration_size
+	local max_iterations = dlg.data.max_iterations
+	if dlg.data.selection_rectangle == true then
+		brush_type = "square"
+	end
+	if dlg.data.contract == true then
+		while (not sprite.selection.isEmpty) do
+			if (max_iterations > 0 and iterations > max_iterations) then
+				break
+			end
+			iterations = iterations + 1
+			app.command.modifySelection {
+				modifier = "contract",
+				quantity = size_change,
+				brush = brush_type
+			}
+		end
+	else
+		while (sprite.selection.bounds.width < sprite.bounds.width or sprite.selection.bounds.height < sprite.bounds.height) do
+			if (max_iterations > 0 and iterations > max_iterations) then
+				break
+			end
+			iterations = iterations + 1
+			app.command.modifySelection {
+				modifier = "expand",
+				quantity = size_change,
+				brush = brush_type
+			}
+		end
+	end
+		
+	
+	local start_col = app.fgColor
+	local end_col = app.bgColor
+	local h_inc = ((end_col.hue - start_col.hue) / iterations)
+	local s_inc = ((end_col.saturation - start_col.saturation) / iterations)
+	local v_inc = ((end_col.value - start_col.value) / iterations)
+	
+	local undo_num = iterations;
+	while undo_num > 0 do
+		app.command.Undo()
+		undo_num = undo_num - 1
+	end
+	
+	local current_iteration = 0
+	if dlg.data.contract == true then
+		while (not sprite.selection.isEmpty) do
+			if (max_iterations > 0 and current_iteration > max_iterations) then
+				break
+			end
+			app.command.Fill()
+			app.fgColor = Color {
+				hue = app.fgColor.hue + h_inc,
+				saturation = app.fgColor.saturation + s_inc,
+				value = app.fgColor.value + v_inc
+			}
+			app.command.ModifySelection {
+				modifier = "contract",
+				quantity = size_change,
+				brush = brush_type
+			}
+			current_iteration = current_iteration + 1
+		end
+	else
+		app.command.modifySelection {
+			modifier = "expand",
+			quantity = size_change * iterations,
+			brush = brush_type
+		}
+		max_iterations = iterations;
+		while (not sprite.selection.isEmpty) do
+			if (max_iterations > 0 and current_iteration > max_iterations) then
+				break
+			end
+			app.command.Fill()
+			app.fgColor = Color {
+				hue = app.fgColor.hue + h_inc,
+				saturation = app.fgColor.saturation + s_inc,
+				value = app.fgColor.value + v_inc
+			}
+			app.command.ModifySelection {
+				modifier = "contract",
+				quantity = size_change,
+				brush = brush_type
+			}
+			current_iteration = current_iteration + 1
+		end
+	end
+	app.fgColor = start_col
+	app.bgColor = end_col
+end
