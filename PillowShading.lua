56c6685893c1d5f5c2f8732a740840378765d481
Drew McIntosh <refreshertowel@gmail.com>
2024-10-25 02:25:12 +1000

    Greater interpolation control
    The pillow shading now allows the user to interpolate between alphas as well. While making this change, I also introduced the ability to toggle interpolation on or off for the Hue, Saturation, Value and Alpha fields.

--- PillowShading.lua
+++ PillowShading.lua
@@ -4,10 +4,10 @@ if sprite.selection.isEmpty then
 end
 
 local data
-local dlg = Dialog():number{ id="max_iterations", label="Max Iterations (0 to perform the maximum possible iterations):", text="0", decimals=0 }
+local dlg = Dialog():number{ id="max_iterations", label="Max Iterations (0 is max possible iterations):", text="0", decimals=0 }
 dlg:modify{ title = "Pillow Shader" }
 dlg:number{ id="iteration_size", label="Iteration size:", text="1", decimals=0 }
-dlg:newrow()
+dlg:newrow{ always=false }
 dlg:radio { id="selection_circle", label = "Selection settings", text = "Circle", selected = true,
 	onclick = function()
 		dlg:modify {
@@ -44,8 +44,14 @@ dlg:radio { id="expand", label="", text="Expand", selected = false,
 		}
 	end
 }
-dlg:button{ id="confirm", text="Confirm" }
-dlg:button{ id="cancel", text="Cancel" }
+dlg:newrow()
+dlg:check { id="hue_interp", label = "Interpolate in (Hue, Sat, Val, Alpha)", text = "h", selected = true }
+dlg:check { id="sat_interp", text = "s", selected = true }
+dlg:check { id="val_interp", text = "v", selected = true }
+dlg:check { id="alpha_interp", text = "a", selected = true }
+dlg:newrow()
+dlg:button { id="confirm", text="Confirm" }
+dlg:button { id="cancel", text="Cancel" }
 dlg:show()
 data = dlg.data
 if data.confirm then
@@ -60,6 +66,7 @@ if data.confirm then
 	if data.selection_rectangle == true then
 		brush_type = "square"
 	end
+	
 	if data.contract == true then
 		while (not sprite.selection.isEmpty) do
 			if (max_iterations > 0 and iterations > max_iterations) then
@@ -89,9 +96,23 @@ if data.confirm then
 	
 	local start_col = app.fgColor
 	local end_col = app.bgColor
-	local h_inc = ((end_col.hue - start_col.hue) / iterations)
-	local s_inc = ((end_col.saturation - start_col.saturation) / iterations)
-	local v_inc = ((end_col.value - start_col.value) / iterations)
+	local iter = iterations - 1
+	local h_inc = 0
+	local s_inc = 0
+	local v_inc = 0
+	local a_inc = 0
+	if (data.hue_interp) then
+		h_inc = ((end_col.hue - start_col.hue) / iter)
+	end
+	if (data.sat_interp) then
+		s_inc = ((end_col.saturation - start_col.saturation) / iter)
+	end
+	if (data.val_interp) then
+		v_inc = ((end_col.value - start_col.value) / iter)
+	end
+	if (data.alpha_interp) then
+		a_inc = ((end_col.alpha - start_col.alpha) / iter)
+	end
 	
 	local undo_num = iterations;
 	while undo_num > 0 do
@@ -107,9 +128,10 @@ if data.confirm then
 			end
 			app.command.Fill()
 			app.fgColor = Color {
-				hue = app.fgColor.hue + h_inc,
-				saturation = app.fgColor.saturation + s_inc,
-				value = app.fgColor.value + v_inc
+				hue 		= app.fgColor.hue + h_inc,
+				saturation 	= app.fgColor.saturation + s_inc,
+				value 		= app.fgColor.value + v_inc,
+				alpha 		= app.fgColor.alpha + a_inc
 			}
 			app.command.ModifySelection {
 				modifier = "contract",
@@ -131,9 +153,10 @@ if data.confirm then
 			end
 			app.command.Fill()
 			app.fgColor = Color {
-				hue = app.fgColor.hue + h_inc,
-				saturation = app.fgColor.saturation + s_inc,
-				value = app.fgColor.value + v_inc
+				hue 		= app.fgColor.hue + h_inc,
+				saturation 	= app.fgColor.saturation + s_inc,
+				value 		= app.fgColor.value + v_inc,
+				alpha 		= app.fgColor.alpha + a_inc
 			}
 			app.command.ModifySelection {
 				modifier = "contract",
